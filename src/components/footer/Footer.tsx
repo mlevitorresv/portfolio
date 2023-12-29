2a3cfbac1183b2c9ed55f2a4cdea6e43495fdd1d
mlevitorresv <levitorres66@gmail.com>
2023-12-29 12:34:23 +0100

    FIX: footer complete

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,13 +5,17 @@ import { ImagesContainerStyled } from '../common/ImagesContainerStyled'
 import { ImageStyled } from '../common/ImageStyled'
 
 export const Footer = () => {
-  return (
-    <FooterStyled>
-        <FooterTitleStyled>Copyright By Manuel Leví Torres Vicente - 2023</FooterTitleStyled>
-        <ImagesContainerStyled>
-        <ImageStyled size='small' src='src\assets\github.png' alt='Github icon'></ImageStyled>
-        <ImageStyled size='small' src='src\assets\linkedin.png' alt='Linkedin icon'></ImageStyled>
-        </ImagesContainerStyled>
-    </FooterStyled>
-  )
+    return (
+        <FooterStyled>
+            <FooterTitleStyled>Copyright By Manuel Leví Torres Vicente - 2023</FooterTitleStyled>
+            <ImagesContainerStyled>
+                <a href='https://github.com/mlevitorresv/'>
+                    <ImageStyled size='small' src='src\assets\github.png' alt='Github icon' />
+                    </a>
+                <a href="https://www.linkedin.com/in/manuel-lev%C3%AD-torres-vicente-abb347241/">
+                    <ImageStyled src='src\assets\linkedin.png' alt='Linkedin icon' size='small' />
+                </a>
+            </ImagesContainerStyled>
+        </FooterStyled>
+    )
 }
