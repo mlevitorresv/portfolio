603d195a2d5702a43d278d231f9ddec98cd1c7b9
mlevitorresv <levitorres66@gmail.com>
2024-11-05 12:42:57 +0100

    ficheros de gilber añadidos

--- src/interfaces/projectSliceInterface.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectInterface } from "./componentsInterface"
-
-export interface ProjectSliceInterface {
-    data: ProjectInterface[],
-    status: 'idle' | 'pending' | 'fulfilled' | 'rejected',
-    error: string | undefined
-}
