a048f077a2b9430ed5f016f72663b51ed2f705e7
mlevitorresv <levitorres66@gmail.com>
2024-11-13 15:58:57 +0100

    ñadido gestor de emailjs, no funciona por conflicto con jquery.3.5.1

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,17 @@
-const downloadCV = () => {
-    const url = '../docs/CV.pdf';
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `Mlevitorresv.pdf`
-    link.click();
-    document.body.removeChild(link);
-}
+import emailjs from 'emailjs-com';
+
+// Inicializar EmailJS con tu User ID
+emailjs.init('TU_USER_ID'); // Reemplaza 'TU_USER_ID' con tu user ID de EmailJS
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    emailjs.sendForm('service_gt3yppm', 'template_pn3pwva', this)
+        .then((response) => {
+            console.log('Correo enviado exitosamente!', response.status, response.text);
+            alert("¡Mensaje enviado con éxito!");
+        }, (error) => {
+            console.error('Error al enviar el mensaje:', error);
+            alert("Hubo un error al enviar el mensaje. Intenta nuevamente.");
+        });
+});
