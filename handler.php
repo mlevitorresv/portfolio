69f1b923bf2b1527b156960379a9f77c4e79a8f4
mlevitorresv <levitorres66@gmail.com>
2024-11-20 14:32:54 +0100

    prueba para mail()

--- handler.php
+++ handler.php
@@ -17,37 +17,39 @@ $mail_to_name = 'MlevitorresV';
 
 try {
 
-	$mail_from_name = isset( $_POST['name'] ) ? $_POST['name'] : '';
-	$mail_from_email = isset( $_POST['email'] ) ? $_POST['email'] : '';
-	$mail_message = isset( $_POST['message'] ) ? $_POST['message'] : '';
+	// $mail_from_name = isset( $_POST['name'] ) ? $_POST['name'] : '';
+	// $mail_from_email = isset( $_POST['email'] ) ? $_POST['email'] : '';
+	// $mail_message = isset( $_POST['message'] ) ? $_POST['message'] : '';
 
 
-	// Server settings
-	$mail->isSMTP(); // Send using SMTP
-	$mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
-	$mail->SMTPAuth = true; // Enable SMTP authentication
-	$mail->Username = $_ENV['EMAIL']; // SMTP username
-	$mail->Password = $_ENV['PASSWORD']; // SMTP password
-	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+	// // Server settings
+	// $mail->isSMTP(); // Send using SMTP
+	// $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+	// $mail->SMTPAuth = true; // Enable SMTP authentication
+	// $mail->Username = $_ENV['EMAIL']; // SMTP username
+	// $mail->Password = $_ENV['PASSWORD']; // SMTP password
+	// $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+	// $mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-	$mail->setFrom($mail_from_email, $mail_from_name); // Your email
-	$mail->addAddress($mail_to_email, $mail_to_name); // Add a recipient
+	// $mail->setFrom($mail_from_email, $mail_from_name); // Your email
+	// $mail->addAddress($mail_to_email, $mail_to_name); // Add a recipient
 
-	// for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
-	// 	$mail->AddAttachment($_FILES['file_attach']['tmp_name'][$ct], $_FILES['file_attach']['name'][$ct]);
-	// }
+	// // for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
+	// // 	$mail->AddAttachment($_FILES['file_attach']['tmp_name'][$ct], $_FILES['file_attach']['name'][$ct]);
+	// // }
 
-	// Content
-	$mail->isHTML(true); // Set email format to HTML
+	// // Content
+	// $mail->isHTML(true); // Set email format to HTML
 
-	$mail->Subject = 'Email desde portfolio' . $mail_from_email;
-	$mail->Body = '
-		<strong>Name:</strong> ' . $mail_from_name . '<br>
-		<strong>Email:</strong> ' . $mail_from_email . '<br>
-		<strong>Message:</strong> ' . $mail_message;
+	// $mail->Subject = 'Email desde portfolio' . $mail_from_email;
+	// $mail->Body = '
+	// 	<strong>Name:</strong> ' . $mail_from_name . '<br>
+	// 	<strong>Email:</strong> ' . $mail_from_email . '<br>
+	// 	<strong>Message:</strong> ' . $mail_message;
 
-	$mail->Send();
+	// $mail->Send();
+	mail('levitorres66@gmail.com', 'Prueba', 'Esto es un mensaje de prueba');
+
 
 	echo 'Message has been sent';
 
