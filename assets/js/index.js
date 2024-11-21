79baa1347c4b5944c99c81a35c6b8bee939bd61a
mlevitorresv <levitorres66@gmail.com>
2024-11-21 12:28:21 +0100

    merge de mlevitorresv

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const downloadCV = () => {
-    const url = '../assets/docs/CV.pdf';
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `Mlevitorresv.pdf`
-    link.click();
-    document.body.removeChild(link);
-}
